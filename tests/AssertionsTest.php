9eb0829e51a3eba0aa999152a7be435faebb0f48
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2021-09-24 23:24:40 +0100

    add assertions

---
+++ tests/AssertionsTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+it('asserts true is true', function () {
+    $this->assertTrue(
+        condition: true
+    );
+
+    expect(
+        value: true
+    )->toBeTrue();
+});
+
+it('asserts false is false', function () {
+    $this->assertFalse(
+        condition: false
+    );
+
+    expect(
+        value: false
+    )->toBeFalse();
+});
+
+it('asserts array to be an array', function () {
+    $array = [1, 2, 3, 4];
+
+    expect(
+        value: $array
+    )->toBeArray();
+});
+
+it('asserts count is four', function () {
+    $array = [1, 2, 3, 4];
+
+    $this->assertCount(
+        expectedCount: 4,
+        haystack: $array
+    );
+});
+
+it('asserts array is equal', function () {
+    $array = [1, 2, 3, 4];
+
+    $this->assertEquals(
+        expected: [1, 2, 3, 4],
+        actual: $array
+    );
+});
+
+it('asserts array is empty', function () {
+    $array = [];
+
+    $this->assertEmpty(
+        actual: $array
+    );
+});
+
+it('asserts string contains string', function () {
+    $this->assertStringContainsString(
+        needle: 'Star',
+        haystack: 'Star Wars'
+    );
+});
